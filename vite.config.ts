8d35fd5b7077121f00316e7f46c014f43f0ec1b3
Santiago Cézar <santiagocezar2013@gmail.com>
2025-05-08 18:11:52 -0300

    await

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		target: 'es2022' // para usar el top-level await
+	},
 	test: {
 		workspace: [
 			{
